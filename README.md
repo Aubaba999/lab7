4209250650044b6d7c29ac838aa0686360f06db0
Aubaba999 <sutjii1989@gmail.com>
2024-12-03 18:46:14 +0700

    lab

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# NAME:
-# STUDENT ID:
+# NAME:kasaemsak suttajai
+# STUDENT ID:670610675
--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,19 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
 char before(char x){
-	//Write your function definition here
-}
+ if(x=='A'){
+     return 'Z';
+ }
+ 
+ if(x>'A'&&x<='Z'){
+     return x-1;
 
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+ }else{
+     return '0';
+ }
 }
