a716ef1f00363fd14c1f236ab25740d4c01afdf9
Aubaba999 <sutjii1989@gmail.com>
2024-12-04 00:30:07 +0700

    b

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -8,31 +8,32 @@ int main() {
     getline(cin,name);
     cout<<"Fahsai: Wow!!! "<<name<<" is a really cool name.\n";
     cout<<"Fahsai: I think you are an Engineering student. What is your student ID?\n";
-    cout<<name<<":";
+    cout<<name<<": ";
     
     int ID ;
     cin>>ID;
     cin.ignore();
-    cout<<"Fahsai: I think you may be GEAR" <<(ID/10000000)-12<<"."<<"I have a free movie ticket for you.\n";
+    cout<<"Fahsai: I think you may be GEAR " <<(ID/10000000)-12<<"."<<" I have a free movie ticket for you.\n";
     cout<<"Fahsai: Let's go to the cinema together!!!\n";
     cout<<"Fahsai: What movie do you want to watch?\n";
-    cout<<name<<":";
+    cout<<name<<": ";
     
     
     string movie;
     getline(cin,movie);
     cout<<"Fahsai: So....which day are you free to go with me?\n";
-    cout<<name<<":";
+    cout<<name<<": ";
     
     
     string Day;
     getline(cin,Day);
     cout<<"Fahsai: "<<Day<<"....that is OK!!! I'm looking forward to watching "<<movie<<" with you.\n";
-    cout<<name<<":";
+    cout<<name<<": ";
     
     string say;
     cin>>say;
-    cout<<"Fahsai: 555+ see you "<<say<<"."<< "Bye Bye \(^ ^)/";
+    cout<<"Fahsai: 555+ see you "<<Day<<"."<< " Bye Bye \\(^ ^)/";
     
    
     return 0;
+}
